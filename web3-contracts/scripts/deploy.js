d3de5a7bf0ff55916d1ca7665b457681f009a8f7
Aryan <aryanamittiwari@gmail.com>
2026-01-13 22:32:27 +0530

    Update

---
+++ web3-contracts/scripts/deploy.js
@@ -0,0 +1,20 @@
+const hre = require("hardhat");
+
+async function main() {
+  console.log("Deploying MediVault contract...");
+
+  const MediVault = await hre.ethers.getContractFactory("MediVault");
+
+  const medivault = await MediVault.deploy();
+
+  await medivault.waitForDeployment();
+
+  console.log("MediVault deployed to:", medivault.target);
+}
+
+// Handle errors
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
